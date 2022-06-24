e3ec801ef95f35f74fe15f94f13f51ddb2c79170
mmartin440 <105955175+mmartin440@users.noreply.github.com>
2022-06-23 18:10:44 -0700

    ChagesMartha

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Your JS goes here
+function checker() {
+  var body = document.querySelector("body");
+  //Each tile should be a div
+  //Each tile's width is 11.1%
+  //Set each tile's float property to left
+  //Each tile's paddingBottom is 11.1%
+  for (var row = 1; row <= 8; row++) {
+    for (var col = 1; col <= 8; col++) {
+      var tile = document.createElement("div");
+      tile.style.width = "12.5%";
+      tile.style.float = "left";
+      tile.style.paddingBottom = "12.5%";
+      if (row % 2 === 0 && col % 2 === 0) {
+        tile.style.background = "black";
+      } else if (row % 2 === 1 && col % 2 === 1) {
+        tile.style.background = "black";
+      } else {
+        tile.style.background = "red";
+      }
+      body.append(tile);
+    }
+  }
+}
+
+checker();
